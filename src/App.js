9dcf9248d78b50b0b2ddeb790db19e27ad7096c4
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-18 20:34:09 +0200

    Создали state в app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Burger from "./Components/burger";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    ingredients: []
+  };
+
     render() {
         return (
             <Fragment>
