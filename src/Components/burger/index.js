a493dda8df5ec4f8f1c347798c335d640bfba551
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-19 11:25:31 +0200

    Добавили компопнент bacon

--- src/Components/burger/index.js
+++ src/Components/burger/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import "./style.css";
+import Meat from "../meat";
 
-const Burger = () => {
+const Burger = ({ingredients}) => {
     return (
         <div className="burgerBlock">
             <div className="BreadTop">
                 <div className="Seeds1"></div>
                 <div className="Seeds2"></div>
             </div>
+            {ingredients.map(ing => {
+                let arr = [];
+                if(ing.name === 'Meat') {
+                    for(let i = 0; i < ing.count; i++) {
+                        arr.push(<Meat/>)
+                    }
+                    return arr;
+                }
+            })}
             <div className="BreadBottom"></div>
         </div>
     );
