ee77763a4847e70cf2cef732475171de478b8395
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-19 10:17:44 +0200

    Добавлена разметка булок бургера

--- src/Components/burger/index.js
+++ src/Components/burger/index.js
@@ -4,7 +4,11 @@ import "./style.css";
 const Burger = () => {
     return (
         <div className="burgerBlock">
-            
+            <div className="BreadTop">
+                <div className="Seeds1"></div>
+                <div className="Seeds2"></div>
+            </div>
+            <div className="BreadBottom"></div>
         </div>
     );
 };
